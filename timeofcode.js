15f2d27a5f36244c2e430ea151e8334e5857dd7b
Adnan <87126125+Adnan42@users.noreply.github.com>
2024-06-12 21:02:14 +0600

    learn code complexity

--- timeofcode.js
+++ timeofcode.js
@@ -1,11 +1,20 @@
 // Big O Notation
 
-function sumofall(n) {
-    let sum = 0;
-    for (let i = 1; i <= n; i++) {
-        sum += i;
-    }
-    return sum;
+// function sumofall(n) {
+//     let sum = 0;
+//     for (let i = 1; i <= n; i++) {
+//         sum += i;
+//     }
+//     return sum;
 
-}
+// }
+
+
+// function sumofall(n){
+//     return n*(n+1)/2;
+// }
+
+
+
+
 console.log(sumofall(10));
