8e1fe30790eddec0c7ff6d35554a4a415f74bc3f
MirInjamamul <injuamul120221@gmail.com>
2024-07-12 17:12:16 +0600

    solve some array's question

---
+++ array.js
@@ -0,0 +1,64 @@
+// all questions are from https://testbook.com/interview/javascript-array-interview-questions   this website...........
+
+
+// Question 1: How do you create an empty array in JavaScript?
+
+//  let arr = [];
+//  let arr1 = new Array();
+//  console.log(arr,arr1);
+
+// Question 2: How do you access the first and last elements of an array?
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let lastElement = arr[arr.length - 1];
+// let firstElement = arr[0];
+// console.log(firstElement, lastElement);
+
+// console.log(arr[0], arr[arr.length-1]);
+// console.log(arr.at(0), arr.at(-1));
+
+// let first = arr.shift();
+// let last = arr.pop();
+//  console.log(first, last);
+
+// Question 3: How do you add an element to the beginning and end of an array?
+
+// Question 4: How do you add an element to the end of an array?
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// arr.push(11);
+// console.log(arr);
+
+
+// Question 5: How do you remove the last element from an array?
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(arr);
+// arr.pop(arr.length-1);
+// console.log(arr);
+
+
+// Question 6: How do you loop through an array in JavaScript?
+
+// Answer: You can loop through an array using either a for loop or a forEach() method. For example:
+
+// Using a for loop:
+// let arr = [1, 2, 3];
+
+// for (let i = 0; i < arr.length; i++) {
+
+// console.log(arr[i]);
+
+// }
+
+// Using the forEach() method:
+
+// let arr = [1, 2, 3];
+
+// arr.forEach(function(element) {
+
+// console.log(element);
+
+// });
+
